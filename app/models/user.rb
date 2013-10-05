6ffcbe3357c8de7e77241c066106aa1182e7ce70
Mike Adeleke <madeleke13@gmail.com>
2013-10-04 19:18:37 -0500

    Added password and encrypted attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,18 +2,21 @@
 #
 # Table name: users
 #
-#  id         :integer          not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer          not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
 #
 
 class User < ActiveRecord::Base
+	attr_accessor :password
 	def user_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
     validates :name,  presence: true, length: { maximum: 50 }
     validates :email, presence: true
-
+    validates :password, presence: true,
+    										 confirmation: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131004231630) do
+ActiveRecord::Schema.define(version: 20131005001513) do
 
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "encrypted_password"
   end
 
 end
